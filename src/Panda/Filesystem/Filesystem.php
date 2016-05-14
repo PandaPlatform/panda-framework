2d605e2e648a606e1ce0711bdcc8c3df704c51ee
Ioannis Papikas <papikas.ioan@gmail.com>
2016-05-14 14:41:58 +0200

    Cleaning up and setting up new classes.

---
+++ src/Panda/Filesystem/Filesystem.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Panda framework Filesystem component.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Panda\Filesystem;
+
+/**
+ * Filesystem handler
+ *
+ * Creates, edits and deletes files.
+ *
+ * @version    0.1
+ */
+class Filesystem
+{
+}
+
+?>
---
+++ src/Panda/Foundation/Application.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Panda framework Foundation component.
+ *
+ * (c) Ioannis Papikas <papikas.ioan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace Panda\Foundation;
+
+/**
+ * Application foundation manager.
+ *
+ * @version    0.1
+ */
+class Application
+{
+    public function __construct()
+    {
+    }
+}
+
+?>
